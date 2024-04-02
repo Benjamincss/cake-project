399c71011b83ede36f437df08899c0f8113ed82f
Benjamincss <benjamin.renouf@outlook.fr>
2024-04-02 15:22:45 +0200

    T1 Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [prenom, setPrenom] = useState('');
+  const [message, setMessage] = useState('');
+  const [erreur, setErreur] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (prenom.trim() === '') {
+      setErreur('Le champ est obligatoire');
+    } else {
+      setMessage(`Bonjour ${prenom}`);
+      setPrenom('');
+      setErreur('');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <button onClick={() => alert('Connectez vous')}>Connectez vous</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+        />
+        <button type="submit">Accéder à votre espace</button>
+      </form>
+      {erreur && <p style={{ color: 'red' }}>{erreur}</p>}
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
 
-export default App
+export default App;
