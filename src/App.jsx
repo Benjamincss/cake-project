6f9a97b0ef6e45aa7ad501122ffdb959ddd33b84
Benjamincss <benjamin.renouf@outlook.fr>
2024-04-03 09:47:55 +0200

    T2 All Feature and Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import React, { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ErrorPage from "./component/ErrorPage";
+import  LoginPage  from "./component/LoginPage";
+import  OrderPage  from "./component/OrderPage";
 
-const App = () => {
-  const [prenom, setPrenom] = useState('');
-  const [message, setMessage] = useState('');
-  const [erreur, setErreur] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (prenom.trim() === '') {
-      setErreur('Le champ est obligatoire');
-    } else {
-      setMessage(`Bonjour ${prenom}`);
-      setPrenom('');
-      setErreur('');
-    }
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <button onClick={() => alert('Connectez vous')}>Connectez vous</button>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Entrez votre prénom..."
-          value={prenom}
-          onChange={(e) => setPrenom(e.target.value)}
-        />
-        <button type="submit">Accéder à votre espace</button>
-      </form>
-      {erreur && <p style={{ color: 'red' }}>{erreur}</p>}
-      {message && <p>{message}</p>}
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order/:username" element={<OrderPage />} />
+      <Route path="/*" element={<ErrorPage />} />
+    </Routes>
   );
-};
+}
 
 export default App;
